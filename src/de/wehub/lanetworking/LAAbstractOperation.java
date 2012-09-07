f6fb4159b6eaf3fcc26b16ae7e7fb0a2811f1b93
batschz <huberwerner@gmail.com>
2012-09-07 21:39:51 +0200

    - minor fixes

--- src/de/wehub/lanetworking/LAAbstractOperation.java
+++ src/de/wehub/lanetworking/LAAbstractOperation.java
@@ -4,7 +4,6 @@ import java.io.ByteArrayOutputStream;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.auth.AuthScope;
-import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CredentialsProvider;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 
--- src/de/wehub/lanetworking/LAHTTPClient.java
+++ src/de/wehub/lanetworking/LAHTTPClient.java
@@ -1,8 +1,22 @@
 package de.wehub.lanetworking;
 
+import org.apache.http.conn.ClientConnectionManager;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.HttpParams;
 
 public class LAHTTPClient extends DefaultHttpClient {
+	
+	public LAHTTPClient() {
+		super();
+	}
+
+	public LAHTTPClient(ClientConnectionManager conman, HttpParams params) {
+		super(conman, params);
+	}
+
+	public LAHTTPClient(HttpParams params) {
+		super(params);
+	}
 
 	private static LAHTTPClient _instance = null;
 	
